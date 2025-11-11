b47762328c6159576d2124699a189d4fba253bc2
jawadgh03 <jawadghaname12@gmail.com>
2025-11-11 17:32:35 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,21 +1,16 @@
 def move(my_list, direction):
-    index_of_one = my_list.index(1)
-    if direction == 'right':
-        if index_of_one == len(my_list) - 1:
-            return my_list
-        my_list[index_of_one] = 0
-        my_list[index_of_one + 1] = 1
-    elif direction == 'left':
-        if index_of_one == 0:
-            return my_list
-        my_list[index_of_one] = 0
-        my_list[index_of_one - 1] = 1
-    return my_list
 
+  index_of_one = my_list.index(1)
 
-def approximate_pi(n):
-    pi_estimate = 0
-    for i in range(n):
-        term = ((-1) ** i) / (2 * i + 1)
-        pi_estimate += term
-    return 4 * pi_estimate
+  
+  if direction == 'right':
+    if   index_of_one < len(my_list)-1 :
+      my_list[index_of_one] = 0
+      my_list[index_of_one + 1] = 1
+
+  elif direction == 'left':
+    if index_of_one > 0:
+      my_list[index_of_one] = 0
+      my_list[index_of_one - 1] = 1
+
+  return my_list
